611f35801153332c23b45286ec21f91bc4de0273
Brendon Zach <brendon_zach@outlook.com>
2022-04-21 14:14:58 -0600

    began writing the code and added things to story board

--- GameReview/ViewController.swift
+++ GameReview/ViewController.swift
@@ -7,9 +7,91 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-    override func viewDidLoad() {
+    @IBOutlet weak var personalComputerImage: UIImageView!
+    
+    @IBOutlet weak var xboxImage: UIImageView!
+    
+    @IBOutlet weak var playstationImage: UIImageView!
+    
+    
+    @IBOutlet weak var textView: UITextView!
+    
+    @IBAction func segmentAction(_ sender: Any)
+    {
+        switch segmentOutlet.selectedSegmentIndex
+        {
+        case 0:
+            textView.text = "Playstation"
+        case 1:
+            textView.text = "Xbox"
+        case 2:
+            textView.text = "PC"
+            
+        
+            
+            
+            
+            
+        
+            
+        default:
+            textView.text = "default"
+            
+        }
+        if segmentOutlet.selectedSegmentIndex == 0
+        {
+            
+            self.view.backgroundColor = UIColor.blue
+            
+        }
+        if segmentOutlet.selectedSegmentIndex == 1
+        {
+            
+            self.view.backgroundColor = UIColor.green
+        }
+        else if segmentOutlet.selectedSegmentIndex == 2
+        {
+            
+            self.view.backgroundColor = UIColor.black
+            
+            
+        }
+        if segmentOutlet.selectedSegmentIndex == 0
+        {
+            playstationImage.isHidden = false
+        }
+        if segmentOutlet.selectedSegmentIndex == 1
+        {
+            playstationImage.isHidden = true
+        }
+        if segmentOutlet.selectedSegmentIndex == 1
+        {
+            xboxImage.isHidden = false
+        }
+        if segmentOutlet.selectedSegmentIndex == 2
+        {
+            personalComputerImage.isHidden = false
+        }
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    @IBOutlet weak var segmentOutlet: UISegmentedControl!
+    
+    
+    
+    
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
